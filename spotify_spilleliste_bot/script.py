b8a620e4d438e0b6aad617640e5bef90703f5616
Fortza <arvidmo12@hotmail.com>
2024-02-04 22:12:36 +0100

    adding comments for clearity

--- spotify_spilleliste_bot/script.py
+++ spotify_spilleliste_bot/script.py
@@ -10,9 +10,12 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 playlist_id = 'https://open.spotify.com/playlist/6RMJRg6d6KTXWPiA5yzRPr?si=42ac1423558e4d04'
 
 #Adding songs 
-songs = ['https://open.spotify.com/track/3AJwUDP919kvQ9QcozQPxg?si=cd1aa9cdbbb24805', 'https://open.spotify.com/track/2cGxRwrMyEAp8dEbuZaVv6?si=f2bb432e0e2b4b89']
+# manually -> 
+## Next version should get songs via spotify urls 
+songs = ['https://open.spotify.com/track/3AJwUDP919kvQ9QcozQPxg?si=cd1aa9cdbbb24805', 'https://open.spotify.com/track/2cGxRwrMyEAp8dEbuZaVv6?si=f2bb432e0e2b4b89'] 
 song_ids = []
 
+# looping through songs added
 for song_url in songs:
     song_id = song_url.split('/')[-1]
     if song_id.startswith('track'):
