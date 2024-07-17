4f68b310a99474dc548cf5f3ad7b1cd16302aa5a
arvid moemeni <arvidmoe@stud.ntnu.no>
2024-07-17 15:49:37 +0200

    added album print function

--- SpotifyAPI/main.py
+++ SpotifyAPI/main.py
@@ -7,6 +7,7 @@
 # 6. Finner mest populære artisten innen Norge for det navnet som brukeren skriver inn. Må ikke være helt riktig.
 # 7. Skriver ut TOP 10 mest populære sanger for Artisten via funksjonen get_top_track_by_artist()
 # 8. Om artisten ikke blir funnet får brukeren en melding før programmet avsluttes
+# 9. Skriver ut de siste 10 album fra artisten og deres realease date
 
 import os # operativsystem funksjoner, i dette tilfellet tilgang til .env 
 import requests #HTTP requests, brukes for å kommunisere med Spotify Web API
@@ -36,17 +37,17 @@ def get_token():
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
     
     url = "https://accounts.spotify.com/api/token"
-    headers = {
+    headers = { # required header parametere
         "Authorization":"Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
-    data = { "grant_type": "client_credentials"  }
+    data = { "grant_type": "client_credentials" } #required body parametere
 
-    result = post(url, headers=headers, data=data) # Nå som alle variabler i curl'en er definert legger vi det inn i result
+    result = post(url, headers=headers, data=data) # Nå som alle variabler i requesten er definert legger vi det inn i result og POST
     json_result = json.loads(result.content) # får tilbake en json, vi ønsker å se på innholdet
     token = json_result["access_token"] #spesifiserer at vi ønsker å se på access_token delen av json objektet og legge det inn i token
-    return token #returnerer token som er det eneste vi ønsker av json egentlig :) 
+    return token #returnerer access token som er det eneste vi ønsker av json objektet! 
    
 
 
@@ -65,7 +66,7 @@ def search_for_artist(token, artist_name):
     result = get(query_url, headers=headers)
     json_result = json.loads(result.content)["artists"]["items"]
     if len(json_result) == 0:
-        print("\n No artist with this name exists SORRY ! \n")
+        print("\n No artist with this name exists SORRY! \n")
         sys.exit()
 
     return json_result[0] 
@@ -79,17 +80,30 @@ def get_top_track_by_artist(token, artists_id):
     return json_result
 
 
+def get_artist_album(token, artists_id):
+    url = f"https://api.spotify.com/v1/artists/{artists_id}/albums?offset=0&limit=10"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result['items']
+
+
 token = get_token() #Kjører gettoken for å hente access token fra Spotify
 result = search_for_artist(token, navn)
 artists_id = result["id"] #nå har vi endelig iD til artisten som vi har søkt opp, denne kan brukes for andre funksjoner hos Spotify
 print("\n")
 print(result["name"])
 print("\n")
+album = get_artist_album(token, artists_id)
 songs = get_top_track_by_artist(token, artists_id)
-#print(songs)
 print ("Top 10 songs on Spotify by", str.upper(result["name"]))
 
 
 for idx, song in enumerate(songs):
     print(f"{idx + 1 }. {song['name']}")
 
+if album:
+    print(f"\nAlbums by {result['name']}:")
+    for idx, album in enumerate(album):
+        release_year = album['release_date'].split('-')[0]
+        print(f"{idx + 1}. {album['name']} (Year: {release_year})")
