d089265b84b038103c978b7ccf647976e40c9d0c
arvid moemeni <arvidmoe@stud.ntnu.no>
2024-08-10 15:26:31 +0200

    adding variables to main.tf

--- Azure/Terraform_Basic_Infrastructure_Docker/main.tf
+++ Azure/Terraform_Basic_Infrastructure_Docker/main.tf
@@ -63,7 +63,7 @@ resource "azurerm_network_security_rule" "am_sec_IP_rule" {
   protocol                   = "*"
   source_port_range          = "*"
   destination_port_range     = "*"
-  source_address_prefix      = "var.my_ip_address" #Verdi Hentes fra variabel fil
+  source_address_prefix      = var.my_ip_address #Verdi Hentes fra variabel fil
   destination_address_prefix = "*"
 
   resource_group_name         = azurerm_resource_group.am_rg.name
