81ac3c49e27e3045f0678d3f0371e4ce9a7c35bb
arvid moemeni <arvidmoe@stud.ntnu.no>
2024-08-14 10:50:47 +0200

    backup skript terraform

---
+++ Automatisering/linux_backup_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#lite skript som skal ta backup av terraform filen og sende det til et bestemt område
+
+echo "Starter backup, ikke slå av maskinen"
+
+#Variabler henter riktig lokasjon i linux tre
+Source_Dir="/home/ubuntu/terraform/files"
+Backup_Dir="/home/ubuntu/backup/terraform"
+
+#Opretter backup katalog
+mkdir -p $Backup_Dir
+
+Date=$(date +"%Y%m%d-%H%M%S")
+
+#For alle filer som finnes i banen /sourcedir og som ender på .tf -> gjør ...
+
+for file in $Source_Dir/*.tf; do 
+	filename=$(basename "$file")
+	cp "$file" "$Backup_Dir/${filename%.*}-$Date.tf"
+
+done
+
+echo "Backup fullført til $Backup_Dir"
+
+
+0 6 * * 1-5 
