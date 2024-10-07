079996c0fbc43cb46671ab57438b30176164e9c8
Arsquid <arvidmoe@stud.ntnu.no>
2024-10-07 02:03:03 +0200

    Added back folder, automatic replacement into backup

--- Automatisering/overvaaking.sh
+++ Automatisering/overvaaking.sh
@@ -8,12 +8,13 @@
 
 #Variabler for lokasjon og tid
 mappe=$(pwd) 
+selv=$(basename "$0")
 naTid=$(date +%s) #Unix timestempel 
 bakTid=$((naTid - 86400 )) #Minus 24 timer. 
 
 #Backup Variabler 
 backupPath="$mappe/backup"
-mkdir -p $backupPath #Lage mappe om ikke finnes for backup 
+mkdir -p "$backupPath" #Lage mappe om ikke finnes for backup 
 
 for file in "$mappe"/*; do #iterere gjennom mappa
 	if [ -f "$file" ]; then #sjekk om forløkka stoppet på en fil
@@ -21,22 +22,27 @@ for file in "$mappe"/*; do #iterere gjennom mappa
 		filLaget=$(stat -f "%B" "$file")
 		filnavn=$(basename "$file")
 		
-	#	printf "\n %s" "$filnavn"
-	#	printf "\n %s" "$sist_endret"
-	
+		# Sjekk om filen er skriptet selv, og hopp over den hvis den er det
+       		if [ "$filnavn" == "$selv" ]; then
+            		continue
+        	fi	
 		if [ $sist_endret -ge $bakTid ]; then #Er sist endret større enn 24 timer så har filen blitt endret på siste døgn.
 			lesbarTid=$(date -r "$sist_endret" +"%Y-%m-%d %H:%M%S")	 # Vise tid for endring.
 			lesbarLaget=$(date -r "$filLaget" +"%Y-%m-%d %H:%M%S")   # Vise når den ble laget.
 			# Vis filnavnet og sist endret tid			
 			printf "\nFilnavn: %s \nOpprettet: %s\nSist endret: %s\n" "$filnavn" "$lesbarLaget" "$lesbarTid"
 
-			if [-f "/$backupPath/$filnavn"]; then # Om filen allerede finnes i backup mappen.
-				rm  /$backupPath/$filnavn # Slett gamle filen først
+			# Før vi lager ny backup, fjerner vi eventuelle tidligere versjoner av filen med tidsstempel
+#			filnavn_base="${filnavn%.*}"
+#            		find "$backupPath" -name "${filnavn_base}_*.${filnavn##*.}" -exec rm {} \;
+			if [ -f "$backupPath/$filnavn" ]; then # Om filen allerede finnes i backup mappen.
+				rm  $backupPath/$filnavn # Slett gamle filen først
 			fi	
 			
-			dato=$(date -r "$sist_endret" + "%d-%m") #Dato format for nye filer til backup mappe senere
+			dato=$(date -r "$sist_endret" +"%m-%d--%H:%M") #Dato format for nye filer til backup mappe senere
 			nytt_filnavn="${filnavn%.*}_$dato.${filnavn##*.}"
-			cp $file /$backupPath/$filnavn #Kopierer nye filen nå som backup mappen ikke har gammel versjon.
+			cp "$file" "$backupPath/$nytt_filnavn" #Kopierer ny fil til backup mappen
+
 		fi		
 	fi
 done  
